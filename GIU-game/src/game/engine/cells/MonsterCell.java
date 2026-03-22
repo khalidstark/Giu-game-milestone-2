518989ec147a546004f7366e7d0f4fb67f67c538
Khalid <kosam7761@gmail.com>
2026-03-22 20:41:15 +0200

    edited the multitasker variable name

--- GIU-game/src/game/engine/cells/MonsterCell.java
+++ GIU-game/src/game/engine/cells/MonsterCell.java
@@ -11,7 +11,7 @@ public class MonsterCell extends Cell{
 		this.cellMonster = cellMonster;
 	}
 	
-	public Monster getMonster(){
+	public Monster getCellMonster(){
 		return cellMonster;
 	}
 
--- GIU-game/src/game/engine/monsters/MultiTasker.java
+++ GIU-game/src/game/engine/monsters/MultiTasker.java
@@ -1,20 +1,21 @@
 package game.engine.monsters;
+
 import game.engine.Role;
 
-public class MultiTasker extends Monster{
-   
-	private int normalSpeed;
-	
-	public MultiTasker(String name, String description, Role role, int energy){
-		super(name, description, role, energy);
-		this.normalSpeed = 0;
-	}
-	
-	public int getNormalSpeed(){
-		return normalSpeed;
-	}
-	
-	public void setNormalSpeed(int normalSpeed){
-		this.normalSpeed = normalSpeed;
-	}
+public class MultiTasker extends Monster {
+
+    private int normalSpeedTurns;
+
+    public MultiTasker(String name, String description, Role role, int energy) {
+        super(name, description, role, energy);
+        this.normalSpeedTurns = 0;
+    }
+
+    public int getNormalSpeedTurns() {
+        return normalSpeedTurns;
+    }
+
+    public void setNormalSpeedTurns(int normalSpeedTurns) {
+        this.normalSpeedTurns = normalSpeedTurns;
+    }
 }

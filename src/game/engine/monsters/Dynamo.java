89cfee723e62426db5b0ac48f823cba36cd4cdbd
Khalid <kosam7761@gmail.com>
2026-04-26 09:37:28 +0300

    Update Dynamo logic

--- src/game/engine/monsters/Dynamo.java
+++ src/game/engine/monsters/Dynamo.java
@@ -1,11 +1,26 @@
 package game.engine.monsters;
-
+import game.engine.Constants;
 import game.engine.Role;
-
 public class Dynamo extends Monster {
 	
 	public Dynamo(String name, String description, Role role, int energy) {
 		super(name, description, role, energy);
 	}
+	
+	public void setEnergy(int energy) {
+        int delta = energy - getEnergy();
+        super.setEnergy(getEnergy() + (2*delta));
+	}
+	
+	public void executePowerupEffect(Monster opponentMonster) {
+		  opponentMonster.setFrozen(true);
+			
+	}
+	
+	
+	
+	
+	
+	
 	
 }

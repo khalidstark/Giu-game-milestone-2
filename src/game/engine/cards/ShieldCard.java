d4d58f9803eade80aa603e1533176bfef3549505
Malak Ahmed <malak.hamdymohamed@student.giu-uni.de>
2026-04-27 20:34:30 +0300

    shield card

--- src/game/engine/cards/ShieldCard.java
+++ src/game/engine/cards/ShieldCard.java
@@ -5,7 +5,8 @@ import game.engine.monsters.Monster;
 public class ShieldCard extends Card {
 	
 	public void performAction(Monster player, Monster opponent){
-		
+		player.setShielded(true);
+		opponent.setShielded(false);
 	}
 	public ShieldCard(String name, String description, int rarity) {
 		super(name, description, rarity, true); 
